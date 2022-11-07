5de4b2a78dbdbb8054600fd42765d1eab297b2c8
Stephen-O-Donovan <booklad@gmail.com>
2022-11-07 09:50:04 +0000

    first

---
+++ Basic Exceptions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void mightGoWrong(){
+
+    bool error = true;
+
+    if(error){
+        throw 8;
+    }
+}
+
+int main(){
+    mightGoWrong();
+
+    return 0;
+}
