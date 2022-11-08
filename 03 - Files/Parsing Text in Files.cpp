133e12175e3ae0a2eafbe22e2a3c0b2d660e86f8
Stephen-O-Donovan <booklad@gmail.com>
2022-11-08 13:41:50 +0000

    finished files

---
+++ 03 - Files/Parsing Text in Files.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    ifstream input;
+    string filename = "stats.txt";
+
+    input.open(filename);
+    if(!input.is_open()){
+        return 1;
+    }
+    while(input){
+        string line;
+        getline(input, line, ':');
+
+        int population;
+        input >> population;
+
+        //input.get();
+        input >> ws;
+
+        
+        if(!input) {
+            break;
+        }
+
+        cout << "'" << line << "'" << " -- " << "'" <<  population << "'" << endl;
+
+    }
+    input.close();
+
+    return 0;
+}
